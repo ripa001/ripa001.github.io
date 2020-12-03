b08f71e08d3655d036bc6a26e09bcf3c32a4e94b
Davide Ripanucci <61156923+ripa001@users.noreply.github.com>
2020-12-03 19:01:16 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -3,4 +3,16 @@ $(document).ready(function(){
 		$(".wrapper").toggleClass("open");
 		$(".wrapper").toggleClass("wrapper-animated");
 	})
+	$("#instruction-icon").hover(function(){
+		$("#instruction").toggleClass("d-none");
+		
+	})
+	$("#experience-icon").hover(function(){
+		$("#experience").toggleClass("d-none");
+		
+	})
+	$("#languages-icon").hover(function(){
+		$("#languages").toggleClass("d-none");
+		
+	})
 })
