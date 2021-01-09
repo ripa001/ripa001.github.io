c4b674d274802585cbe9f4fc6b113cb2f99eaee4
Davide Ripanucci <61156923+ripa001@users.noreply.github.com>
2021-01-09 15:43:41 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,24 +1,41 @@
 $(document).ready(function(){
+	$(document).on('scroll', '#about', function() {
+		$(".experience-li").addClass("d-none");
+		$(".instruction-li").addClass("d-none");
+		$(".languages-li").removeClass("d-none");
+		$("#languages").addClass("active-tab")
+		$("#instruction").removeClass("active-tab")
+		$("#experience").removeClass("active-tab")
+	})
 	$(".sidebar").hover(function(){
 		$(".wrapper").toggleClass("open");
 		$(".wrapper").toggleClass("wrapper-animated");
 	})
 	$("#instruction").hover(function(){
-		$("#languages-content").addClass("d-none");
-		$("#experience-content").addClass("d-none");
-		$("#instruction-content").removeClass("d-none");
+		$(".languages-li").addClass("d-none");
+		$(".experience-li").addClass("d-none");
+		$(".instruction-li").removeClass("d-none");
+		$("#instruction").addClass("active-tab")
+		$("#languages").removeClass("active-tab")
+		$("#experience").removeClass("active-tab")
 		
 	})
 	$("#languages").hover(function(){
-		$("#experience-content").addClass("d-none");
-		$("#instruction-content").addClass("d-none");
-		$("#languages-content").removeClass("d-none");
+		$(".experience-li").addClass("d-none");
+		$(".instruction-li").addClass("d-none");
+		$(".languages-li").removeClass("d-none");
+		$("#languages").addClass("active-tab")
+		$("#instruction").removeClass("active-tab")
+		$("#experience").removeClass("active-tab")
 		
 	})
 	$("#experience").hover(function(){
-		$("#languages-content").addClass("d-none");
-		$("#instruction-content").addClass("d-none");
-		$("#experience-content").removeClass("d-none");
+		$(".languages-li").addClass("d-none");
+		$(".instruction-li").addClass("d-none");
+		$(".experience-li").removeClass("d-none");
+		$("#experience").addClass("active-tab")
+		$("#languages").removeClass("active-tab")
+		$("#instruction").removeClass("active-tab")
 		
 	})
 
